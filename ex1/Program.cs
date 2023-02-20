d879de40a1473d4a21b018a8b9b8495b2d5eea58
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-20 21:46:31 +0300

    Добавлена программа по сортировке по убыванию строк двумерного массива

---
+++ ex1/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+int[,] InitMatrix()
+{
+    int[,] array = new int[3, 4];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] matrix, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] RowDescendingSortMatrix(int[,] matrix)
+{
+    int temp1;
+    int temp2;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, j])
+                {
+                    temp1 = matrix[i, k];
+                    temp2 = matrix[i, j];
+                    matrix[i, j] = temp1;
+                    matrix[i, k] = temp2;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix, "Случайный массив целых чисел:");
+int[,] sortMatrix = RowDescendingSortMatrix(matrix);
+PrintMatrix(sortMatrix, "Отсортированный массив:");
