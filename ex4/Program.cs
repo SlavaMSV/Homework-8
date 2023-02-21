f22b482b6f686b4d80336974e111951660622ffc
SlavaMSV <viacheslav.maslov@gmail.com>
2023-02-21 11:13:51 +0300

    Добавлена вывода на экран 3D массива с индексами

---
+++ ex4/Program.cs
@@ -0,0 +1,60 @@
+﻿/* 
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int[,,] InitThreeDArray()
+{
+    int[,,] array = new int[2, 2, 2];
+    Random rnd = new Random();
+    array[0, 0, 0] = rnd.Next(0, 100);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 1; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rnd.Next(0, 100);
+                while (true)
+                {
+                    if (array[i, j, k] != array[i, j, k - 1])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        array[i, j, k] = rnd.Next(0, 100);
+                    }
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void PrintThreeDArray(int[,,] threeDarray)
+{
+    Console.WriteLine("Построчный вывод 3Д массива:");
+    for (int k = 0; k < threeDarray.GetLength(2); k++)
+    {
+        for (int i = 0; i < threeDarray.GetLength(0); i++)
+        {
+            for (int j = 0; j < threeDarray.GetLength(1); j++)
+            {
+                Console.Write($"{threeDarray[i, j, k]} ({i}, {j}, {k})  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] threeDarray = InitThreeDArray();
+PrintThreeDArray(threeDarray);
